d5885726998a25bd2933f3b2893cfdf6f55a60ac
opalala1 <cldi212@naver.com>
2022-12-29 21:46:38 +0900

    수정

--- src/main/java/com/example/sidepot/security/app/AuthService.java
+++ src/main/java/com/example/sidepot/security/app/AuthService.java
@@ -1,24 +1,19 @@
 package com.example.sidepot.security.app;
 
-import com.example.sidepot.global.error.ErrorCode;
-import com.example.sidepot.global.error.Exception;
-import com.example.sidepot.security.error.TokenException;
-import com.example.sidepot.security.util.TokenIssuer;
 import com.example.sidepot.security.domain.Auth;
 import com.example.sidepot.security.domain.AuthRepository;
-import com.example.sidepot.security.dto.AuthDto.TokenDto;
 import com.example.sidepot.security.dto.AuthDto.MemberLoginDto;
+import com.example.sidepot.security.dto.AuthDto.TokenDto;
+import com.example.sidepot.security.error.TokenException;
+import com.example.sidepot.security.util.TokenIssuer;
 import io.jsonwebtoken.Claims;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-import java.util.Optional;
 
 @Service
-public class AuthService implements UserDetailsService {
+public class AuthService {
 
 
     private final String GRANT_TYPE_BEARER = "Bearer";
@@ -56,19 +51,14 @@ public class AuthService implements UserDetailsService {
 
     public TokenDto login(MemberLoginDto memberLoginDto)  {
 
-        Auth user;
-        Optional<Auth> o = authRepository.findByPhone(memberLoginDto.getPhone());
-
-        if(o.isPresent()){ user = o.get(); }
-        else{ throw new Exception(ErrorCode.MEMBER_NOT_FOUND); }
-
-        return createTokenDto(user);
+        Auth auth = authRepository.findByPhone(memberLoginDto.getPhone())
+                .orElseThrow(()->new UsernameNotFoundException("사용자를 찾을 수 없음"));
+        return createTokenDto(auth);
     }
 
     public TokenDto reissue(String token) throws Throwable {
 
         Auth user;
-        Optional<Auth>  o;
 
         String refreshToken = resolveToken(token);
         if (!StringUtils.hasText(refreshToken)) { throw new TokenException(" "); }
@@ -76,16 +66,10 @@ public class AuthService implements UserDetailsService {
         Claims claims = issuer.parseRefreshClaims(refreshToken);
         if (claims == null) { throw new TokenException(" "); }
 
-        o = authRepository.findByPhone(claims.getSubject());
+        user = authRepository.findByPhone(claims.getSubject())
+                .orElseThrow(()->new UsernameNotFoundException("사용자를 찾을 수 없음"));
 
-        if(o.isPresent()){ user = o.get(); }
-        else{throw new Exception(ErrorCode.MEMBER_NOT_FOUND);}
 
         return createTokenDto(user);
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Auth auth = authRepository.findByPhone(username).orElseThrow(()->throw new UsernameNotFoundException("사용자를 찾을 수 없음"));
     }
 }
